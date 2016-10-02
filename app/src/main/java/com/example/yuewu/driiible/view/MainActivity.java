9f3596f2cd34e55798ecaafa6c1d06795bef27d3
Yue Wu <yuriyuewu@gmail.com>
2016-10-02 18:35:24 -0400

    commit navigation bar with toolbar

--- app/src/main/java/com/example/yuewu/driiible/view/MainActivity.java
+++ app/src/main/java/com/example/yuewu/driiible/view/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.drawer) NavigationView navigationView;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.my_toolbar) Toolbar toolBar; // using toolBar
 
     private ActionBarDrawerToggle drawerToggle;
 
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
+        setSupportActionBar(toolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
