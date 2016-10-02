debe711551063413fa0a9a11ccae7797b4f149a7
Yue Wu <yuriyuewu@gmail.com>
2016-10-02 16:27:50 -0400

    commit changes for navigation drawer

--- app/src/main/java/com/example/yuewu/driiible/view/MainActivity.java
+++ app/src/main/java/com/example/yuewu/driiible/view/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.yuewu.driiible.view;
 
+import android.content.res.Configuration;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.drawer) NavigationView navigationView;
     @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
 
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +38,43 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setupDrawer();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,          /* DrawerLayout object */
+                R.string.open_drawer,         /* "open drawer" description */
+                R.string.close_drawer         /* "close drawer" description */
+        );
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -44,17 +85,17 @@ public class MainActivity extends AppCompatActivity {
                 Fragment fragment = null;
                 switch (item.getItemId()){
                     case R.id.drawer_item_home:
-                        Toast.makeText(MainActivity.this, "home clicked", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(MainActivity.this, "home clicked", Toast.LENGTH_LONG).show();
                         fragment = ShotListFragment.newInstance();
                         setTitle(R.string.title_home);
                         break;
                     case R.id.drawer_item_likes:
-                        Toast.makeText(MainActivity.this, "likes clicked", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(MainActivity.this, "likes clicked", Toast.LENGTH_LONG).show();
                         fragment = ShotListFragment.newInstance();
                         setTitle(R.string.title_likes);
                         break;
                     case R.id.drawer_item_buckets:
-                        Toast.makeText(MainActivity.this, "buckets clicked", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(MainActivity.this, "buckets clicked", Toast.LENGTH_LONG).show();
                         fragment = BucketListFragment.newInstance();
                         setTitle(R.string.title_buckets);
                         break;
@@ -72,14 +113,15 @@ public class MainActivity extends AppCompatActivity {
 
                 if (fragment != null) {
                     getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_container, fragment)
-                        .commit();
-                return true;
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                    return true;
                 }
                 return false;
             }
         });
-
     }
+
+
 }
